355ad7c390d2ce5bfa84829afc44bee03609fd28
Raghav-Pal <raghav@Raghavs-MacBook-Pro.local>
2018-08-29 12:45:00 +0530

    Added project automation

---
+++ src/main/java/test/automation/App.java
@@ -0,0 +1,13 @@
+package test.automation;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "\nHello World! Automation step by step\n" );
+    }
+}
